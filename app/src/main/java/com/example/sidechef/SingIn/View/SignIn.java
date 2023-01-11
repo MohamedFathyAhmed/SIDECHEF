7c5831497d28b94ee648448a42ef55e32d8c8834
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-01-11 18:23:17 +0200

    add signin activity

--- app/src/main/java/com/example/sidechef/SingIn/View/SignIn.java
+++ app/src/main/java/com/example/sidechef/SingIn/View/SignIn.java
@@ -3,15 +3,37 @@ package com.example.sidechef.SingIn.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.sidechef.R;
 
 
+
+
 public class SignIn extends AppCompatActivity {
+Button btn_login;
+TextView tv_name;
+TextView tv_pass;
+    private void initUI() {
+        btn_login = findViewById(R.id.btn_login);
+        tv_name = findViewById(R.id.tx_Name);
+        tv_pass = findViewById(R.id.tx_pass);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        initUI();
+
+
     }
+
+
+
+
+
+
+
 }
