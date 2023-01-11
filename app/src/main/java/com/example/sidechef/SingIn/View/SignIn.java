1a45534fe35c2c8c1fd189d2d0c7cd8ecc3b9076
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-01-11 17:53:43 +0200

    add signIn

---
+++ app/src/main/java/com/example/sidechef/SingIn/View/SignIn.java
@@ -0,0 +1,17 @@
+package com.example.sidechef.SingIn.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sidechef.R;
+
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
