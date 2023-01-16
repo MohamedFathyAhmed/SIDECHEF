f9aa6e7036f7fadfa4ede4bfbd4e9cac852f2197
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-01-16 21:51:20 +0200

    Revert "Revert "Merge branch 'home-pranch' into dev""
    This reverts commit 1a8ce8ae18168f768475e3214acb688308e5d9ac.

--- app/src/main/java/com/example/sidechef/HomeActivity/View/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sidechef/HomeActivity/View/ui/home/HomeFragment.java
@@ -5,52 +5,108 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.sidechef.R;
+import com.example.sidechef.model.data.models.Category;
+import com.example.sidechef.model.data.models.Meal;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.sidechef.HomeActivity.View.ui.DetailViewinterface;
 import com.example.sidechef.R;
-import com.example.sidechef.databinding.FragmentHomeBinding;
+import java.util.Arrays;
+import java.util.List;
 import com.example.sidechef.model.data.models.Meal;
 
 public class HomeFragment extends Fragment {
-Button btn;
-    Meal meal;
+        RecyclerView categoryrecyclerView;
+        RecyclerView randomrecyclerView;
+        Button btn;
+        Meal meal;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        private FragmentHomeBinding binding;
 
-    }
+        public View onCreateView(@NonNull LayoutInflater inflater,
+                        ViewGroup container, Bundle savedInstanceState) {
+                HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        btn = view.findViewById(R.id.btngo);
+                binding = FragmentHomeBinding.inflate(inflater, container, false);
+                View root = binding.getRoot();
 
+        }
 
-                btn.setOnClickListener(v->{
-                     meal =new Meal( "52842", "Broccoli & Stilton soup",
-                            null, "Starter", "British", "Heat the rapeseed oil in a large saucepan and then add the onions. Cook on a medium heat until soft. Add a splash of water if the onions start to catch. Add the celery, leek, potato and a knob of butter. Stir until melted, then cover with a lid. Allow to sweat for 5 minutes. Remove the lid. Pour in the stock and add any chunky bits of broccoli stalk. Cook for 10 – 15 minutes until all the vegetables are soft. Add the rest of the broccoli and cook for a further 5 minutes. Carefully transfer to a blender and blitz until smooth. Stir in the stilton, allowing a few lumps to remain. Season with black pepper and serve.", "https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg", null, "https://www.youtube.com/watch?v=_HgVLpmNxTY", "Rapeseed Oil", "Onion", "Celery", "Leek", "Potatoes", "Butter", "Vegetable Stock", "Broccoli",
-                            "Stilton Cheese", "", "", "", "", "", "", "", "", "", "", "", "2 tblsp ", "1 finely chopped ", "1", "1 sliced", "1 medium", "1 knob", "1 litre hot", "1 Head chopped", "140g", "", "", "",
-                            "", "", "", "", "", "", "", "",
-                            "https://www.bbcgoodfood.com/recipes/1940679/broccoli-and-stilton-soup", null, null,null);
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+                super.onViewCreated(view, savedInstanceState);
+                initrandommealRecView(view);
+                initCategoryRecView(view);
 
-                    HomeFragmentDirections.ActionNavigationHomeToDetailFragment action =  HomeFragmentDirections.actionNavigationHomeToDetailFragment();
-                    action.setDataMeal(meal);
-                    Log.i("TAG", "onCreateView: "+meal.getStrArea());
-                    Navigation.findNavController(v).navigate(action);
-                });
+        }
 
+        void initCategoryRecView(View view) {
+                categoryrecyclerView = (RecyclerView) view.findViewById(R.id.category_rec_view);
+                categoryrecyclerView.setHasFixedSize(true);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+                layoutManager.setOrientation(RecyclerView.HORIZONTAL);
+                categoryrecyclerView.setLayoutManager(layoutManager);
+                List arrayList = Arrays.asList(new Category(
+                                "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Female_house_sparrow_at_Kodai.jpg/280px-Female_house_sparrow_at_Kodai.jpg",
+                                "Cake1 Desc"),
+                                new Category("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Female_house_sparrow_at_Kodai.jpg/280px-Female_house_sparrow_at_Kodai.jpg",
+                                                "cacke"));
+                Categoriesadapter mAdapter = new Categoriesadapter(arrayList, view.getContext());
+                categoryrecyclerView.setAdapter(mAdapter);
+        }
 
-    }
+        void initrandommealRecView(View view) {
+                randomrecyclerView = (RecyclerView) view.findViewById(R.id.randomMealrecview);
+                randomrecyclerView.setHasFixedSize(true);
+                LinearLayoutManager layoutManager = new LinearLayoutManager(view.getContext());
+                layoutManager.setOrientation(RecyclerView.VERTICAL);
+                randomrecyclerView.setLayoutManager(layoutManager);
+                List<Meal> arrayList = Arrays.asList(new Meal(Long.getLong("5656565"), "Broccoli & Stilton soup",
+                                null,
+                                "Starter", "British",
+                                "Heat the rapeseed oil in a large saucepan and then add the onions. Cook on a medium heat until soft. Add a splash of water if the onions start to catch. Add the celery, leek, potato and a knob of butter. Stir until melted, then cover with a lid. Allow to sweat for 5 minutes. Remove the lid. Pour in the stock and add any chunky bits of broccoli stalk. Cook for 10 – 15 minutes until all the vegetables are soft. Add the rest of the broccoli and cook for a further 5 minutes. Carefully transfer to a blender and blitz until smooth. Stir in the stilton, allowing a few lumps to remain. Season with black pepper and serve.",
+                                "https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg", null,
+                                "https://www.youtube.com/watch?v=_HgVLpmNxTY", "Rapeseed Oil", "Onion", "Celery",
+                                "Leek", "Potatoes", "Butter", "Vegetable Stock", "Broccoli",
+                                "Stilton Cheese",
+                                "", "", "", "", "",
+                                "", "", "", "", "",
+                                "", "2 tblsp ", "1 finely chopped ", "1",
+                                "1 sliced", "1 medium", "1 knob", "1 litre hot",
+                                "1 Head chopped", "140g", "", "",
+                                "",
+                                "", "", "", "", "", "", "", "",
+                                "https://www.bbcgoodfood.com/recipes/1940679/broccoli-and-stilton-soup",
+                                null, null, null),
+                                new Meal(Long.getLong("52842"), "Broccoli & Stilton soup",
+                                                null, "Starter", "British",
+                                                "Heat the rapeseed oil in a large saucepan and then add the onions. Cook on a medium heat until soft. Add a splash of water if the onions start to catch. Add the celery, leek, potato and a knob of butter. Stir until melted, then cover with a lid. Allow to sweat for 5 minutes. Remove the lid. Pour in the stock and add any chunky bits of broccoli stalk. Cook for 10 – 15 minutes until all the vegetables are soft. Add the rest of the broccoli and cook for a further 5 minutes. Carefully transfer to a blender and blitz until smooth. Stir in the stilton, allowing a few lumps to remain. Season with black pepper and serve.",
+                                                "https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg",
+                                                null, "https://www.youtube.com/watch?v=_HgVLpmNxTY", "Rapeseed Oil",
+                                                "Onion", "Celery", "Leek", "Potatoes", "Butter", "Vegetable Stock",
+                                                "Broccoli",
+                                                "Stilton Cheese", "", "", "", "", "", "", "", "", "", "", "",
+                                                "2 tblsp ", "1 finely chopped ", "1", "1 sliced", "1 medium", "1 knob",
+                                                "1 litre hot", "1 Head chopped", "140g", "", "", "",
+                                                "", "", "", "", "", "", "", "",
+                                                "https://www.bbcgoodfood.com/recipes/1940679/broccoli-and-stilton-soup",
+                                                null, null, null));
+                CardViewAdapter mAdapter = new CardViewAdapter(arrayList, view.getContext());
+                randomrecyclerView.setAdapter(mAdapter);
+        }
+
 }
--- app/src/main/java/com/example/sidechef/SingInActivity/View/SignIn.java
+++ app/src/main/java/com/example/sidechef/SingInActivity/View/SignIn.java
@@ -27,22 +27,20 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.util.Objects;
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener, LoginContract.View {
-Button btn_login;
-EditText tv_name;
+    Button btn_login;
+    EditText tv_name;
     EditText tv_pass;
     LoginPresenter mLoginPresenter;
     ProgressDialog mProgressDialog;
 
-     SignInButton googeSignIn;
-     GoogleSignInOptions gso;
-     GoogleSignInClient gsc;
+    SignInButton googeSignIn;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
 
-     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-
-
-
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     private void initUI() {
         btn_login = findViewById(R.id.btn_register);
@@ -60,30 +58,32 @@ EditText tv_name;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //For google sign in
+        // For google sign in
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
         gsc = GoogleSignIn.getClient(getApplicationContext(), gso);
 
-       setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_in);
+
         initUI();
 
     }
 
-
     private void checkLoginDetails() {
-        if(!TextUtils.isEmpty(tv_name.getText().toString()) && !TextUtils.isEmpty(tv_pass.getText().toString())){
+        if (!TextUtils.isEmpty(tv_name.getText().toString()) && !TextUtils.isEmpty(tv_pass.getText().toString())) {
             initLogin(tv_name.getText().toString(), tv_pass.getText().toString());
-        }else{
-            if(TextUtils.isEmpty(tv_name.getText().toString())){
+        } else {
+            if (TextUtils.isEmpty(tv_name.getText().toString())) {
                 tv_name.setError("Please enter a valid email");
-            }if(TextUtils.isEmpty(tv_pass.getText().toString())){
-               tv_pass.setError("Please enter password");
+            }
+            if (TextUtils.isEmpty(tv_pass.getText().toString())) {
+                tv_pass.setError("Please enter password");
             }
         }
     }
+
     private void initLogin(String email, String password) {
         mProgressDialog.show();
         mLoginPresenter.login(this, email, password);
@@ -91,22 +91,18 @@ EditText tv_name;
 
     @Override
     public void onClick(android.view.View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.btn_register:
                 Log.i("hellplab", "btn_login: ");
-               checkLoginDetails();
+                checkLoginDetails();
                 break;
             case R.id.btn_signInWithGoogle:
-                //Google sign in button
+                // Google sign in button
                 Log.i("hellplab", "btn_login: ");
                 signInGoogle();
                 break;
         }
     }
-
-
-
-
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
@@ -117,7 +113,7 @@ EditText tv_name;
     public void onLoginSuccess(String message) {
         mProgressDialog.dismiss();
         Log.i("hellplab", "onLoginSuccess: ");
-        Toast.makeText(getApplicationContext(), "Successfully Logged in" , Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), "Successfully Logged in", Toast.LENGTH_SHORT).show();
         startActivity(new Intent(this, HomeActivity.class));
     }
 
@@ -125,17 +121,16 @@ EditText tv_name;
     public void onLoginFailure(String message) {
         mProgressDialog.dismiss();
         Log.i("hellplab", "onLoginFailure: ");
-        Toast.makeText(getApplicationContext(),message , Toast.LENGTH_SHORT).show();
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
-    //For google sign in
+    // For google sign in
     void signInGoogle() {
         Intent signInIntent = gsc.getSignInIntent();
         startActivityForResult(signInIntent, 1000);
     }
 
-
-    //callback of sign in with google request (for google sign in)
+    // callback of sign in with google request (for google sign in)
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -144,16 +139,17 @@ EditText tv_name;
             if (task.isSuccessful()) {
                 Toast.makeText(getApplicationContext(), "Sign in was successful", Toast.LENGTH_SHORT).show();
 
-                //Next 2 lines were used to link google sign in with firebase
-                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(task.getResult().getIdToken(), null);
+                // Next 2 lines were used to link google sign in with firebase
+                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(task.getResult().getIdToken(),
+                        null);
                 firebaseAuth.signInWithCredential(firebaseCredential);
 
-//                Intent intent = new Intent(requireContext(), MainActivity.class);
-//                startActivity(intent);
-            }
-            else{
+                // Intent intent = new Intent(requireContext(), MainActivity.class);
+                // startActivity(intent);
+            } else {
                 Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
-                //Toast.makeText(requireContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(requireContext(), "Sign in failed",
+                // Toast.LENGTH_SHORT).show();
             }
 
         }
