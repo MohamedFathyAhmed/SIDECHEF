08ce68463d49b3b995e4a0d7e983ffe1df96ffde
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-01-16 11:40:10 +0200

    add:google buttom auth

--- app/src/main/java/com/example/sidechef/SingInActivity/View/SignIn.java
+++ app/src/main/java/com/example/sidechef/SingInActivity/View/SignIn.java
@@ -17,6 +17,15 @@ import com.example.sidechef.HomeActivity.View.HomeActivity;
 import com.example.sidechef.R;
 import com.example.sidechef.model.firebase.Login.LoginContract;
 import com.example.sidechef.model.firebase.Login.LoginPresenter;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 public class SignIn extends AppCompatActivity implements View.OnClickListener, LoginContract.View {
@@ -26,6 +35,12 @@ EditText tv_name;
     LoginPresenter mLoginPresenter;
     ProgressDialog mProgressDialog;
 
+     SignInButton googeSignIn;
+     GoogleSignInOptions gso;
+     GoogleSignInClient gsc;
+
+     FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
 
 
 
@@ -33,16 +48,26 @@ EditText tv_name;
         btn_login = findViewById(R.id.btn_register);
         tv_name = findViewById(R.id.email_register);
         tv_pass = findViewById(R.id.password_register);
+        googeSignIn = findViewById(R.id.btn_signInWithGoogle);
         mLoginPresenter = new LoginPresenter(this);
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setMessage("Please wait, Logging in..");
         btn_login.setOnClickListener(this);
+        googeSignIn.setOnClickListener(this);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+
+        //For google sign in
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        gsc = GoogleSignIn.getClient(getApplicationContext(), gso);
+
+       setContentView(R.layout.activity_sign_in);
         initUI();
 
     }
@@ -71,8 +96,17 @@ EditText tv_name;
                 Log.i("hellplab", "btn_login: ");
                checkLoginDetails();
                 break;
+            case R.id.btn_signInWithGoogle:
+                //Google sign in button
+                Log.i("hellplab", "btn_login: ");
+                signInGoogle();
+                break;
         }
     }
+
+
+
+
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
@@ -93,4 +127,36 @@ EditText tv_name;
         Log.i("hellplab", "onLoginFailure: ");
         Toast.makeText(getApplicationContext(),message , Toast.LENGTH_SHORT).show();
     }
+
+    //For google sign in
+    void signInGoogle() {
+        Intent signInIntent = gsc.getSignInIntent();
+        startActivityForResult(signInIntent, 1000);
+    }
+
+
+    //callback of sign in with google request (for google sign in)
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1000) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (task.isSuccessful()) {
+                Toast.makeText(getApplicationContext(), "Sign in was successful", Toast.LENGTH_SHORT).show();
+
+                //Next 2 lines were used to link google sign in with firebase
+                AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(task.getResult().getIdToken(), null);
+                firebaseAuth.signInWithCredential(firebaseCredential);
+
+//                Intent intent = new Intent(requireContext(), MainActivity.class);
+//                startActivity(intent);
+            }
+            else{
+                Toast.makeText(getApplicationContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(requireContext(), "Sign in failed", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+    }
+
 }
