9b1762e922702d270bb2b711a97b995381ffa7f8
MohamedFathyAhmed <112295268+MohamedFathyAhmed@users.noreply.github.com>
2023-01-27 11:25:42 +0200

    add :net

--- app/src/main/java/com/example/sidechef/HomeActivity/View/HomeActivity.java
+++ app/src/main/java/com/example/sidechef/HomeActivity/View/HomeActivity.java
@@ -3,7 +3,9 @@ package com.example.sidechef.HomeActivity.View;
 import android.content.ClipData;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
+import com.example.sidechef.MainActivity;
 import com.example.sidechef.R;
 import com.example.sidechef.Utils.Utils;
 import com.example.sidechef.Utils.YourPreference;
@@ -18,11 +20,22 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class HomeActivity extends AppCompatActivity {
-
+     int timerInternetIsConnected ;
     private ActivityHomeBinding binding;
+    BottomNavigationView navBar;
+TextView tv_internetConnection;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +46,48 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Objects.requireNonNull(getSupportActionBar()).hide();
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration;
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_planweek, R.id.navigation_favorite,R.layout.fragment_search,R.layout.fragment_user)
-                .build();
+        Timer t = new Timer( );
+        tv_internetConnection = findViewById(R.id.tx_internet);
+        navBar = findViewById(R.id.nav_view);
+        t.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                if (!Utils.isNetworkAvailable( getBaseContext())){
+                    HomeActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            tv_internetConnection.setVisibility(View.VISIBLE);
+                            tv_internetConnection.setText("No Internet Connection");
+                            tv_internetConnection.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                            timerInternetIsConnected = 5001;
 
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
 
+                            navBar.setVisibility(View.GONE);
+                        }
+                    });
 
+                } else{
+                    HomeActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(timerInternetIsConnected > 0){
+                                timerInternetIsConnected = timerInternetIsConnected - 500;
+                                tv_internetConnection.setBackgroundColor(getResources().getColor(R.color.blue));
+                                tv_internetConnection.setText("Internet is back!");
+                            } else{
+                                tv_internetConnection.setVisibility(View.GONE);
+                                navBar.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        }, 1000,500);
 
     }
+
+
 
 }
--- app/src/main/java/com/example/sidechef/SplashScreen.java
+++ app/src/main/java/com/example/sidechef/SplashScreen.java
@@ -15,37 +15,38 @@ import com.example.sidechef.Utils.Utils;
 
 
 public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = getSharedPreferences("sharedPreferences", 0);
+                String value = preferences.getString("first_time", null);
+                if (value == null) {
+                    startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                    SharedPreferences.Editor pre = preferences.edit();
+                    pre.putString("first_time", "no");
+                    pre.apply();
+                } else {
+                    if (preferences.getString("email", "guest").equals("guest")) {
+                        startActivity(new Intent(SplashScreen.this, SignUp.class));
+                    } else {
+                        startActivity(new Intent(SplashScreen.this, HomeActivity.class));
+                    }
+
+                }
+            }
+        }, 0);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-     //   if (!Utils.isNetworkAvailable(getApplicationContext())) {
 
-//alert here if no net
 
-      //  } else {
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    SharedPreferences preferences = getSharedPreferences("sharedPreferences", 0);
-                    String value = preferences.getString("first_time", null);
-                    if (value == null) {
-                        startActivity(new Intent(SplashScreen.this, MainActivity.class));
-                        SharedPreferences.Editor pre = preferences.edit();
-                        pre.putString("first_time", "no");
-                        pre.apply();
-                    } else {
-                        if (preferences.getString("email", "guest").equals("guest")) {
-                            startActivity(new Intent(SplashScreen.this, SignUp.class));
-                        } else {
-                            startActivity(new Intent(SplashScreen.this, HomeActivity.class));
-                        }
 
-                    }
-                }
-            }, 0);
 
-     //   }
     }
 }
